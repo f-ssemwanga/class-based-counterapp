722e2d7dcb7453668a0cbba155699255bde4567e
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 07:02:50 +0100

    simple app stage with correct render and imports

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+export const App = () =>{
+   //add a return statement
+    return (
+    <div>
+        App
+    </div>
+)}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import App from './App';
+import {App} from './App';
 
-
-
+//create a root object
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//render the app
+root.render(<App/>)
