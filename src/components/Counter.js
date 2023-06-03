066c7cddd4c3632ceb987c12c257914b11a01ab1
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 09:31:42 +0100

    complete styling of the counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -15,7 +15,16 @@ export class Counter extends Component{
     //render method
 
     render(){
-        return <div> Counter content </div>;
+        return <div className="border m-3 p-4 rounded"> 
+          <div className="sub-title text-center m-5 fs-2">Counter: <span className="text-primary fs-2 p-2 fw-bold">0</span></div>
+          <div className="d-flex justify-content-evenly flex-column gap-3">
+              <button className ="btn btn-success fs-3">-</button>
+              <button className ="btn btn-danger">Reset</button>
+              <button className ="btn btn-success fs-3">+</button> 
+          </div>
+                              
+          
+          </div>;
     }
 
 }
