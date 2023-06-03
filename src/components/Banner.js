f58ba4aef07f23aa15188bbba35df25040cd86e3
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 09:07:16 +0100

    Banner component complete

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,9 @@
 export const Banner = ()=>{
 
     return(
-        <div>
-            Banner content
-        </div>
+        <div class="p-3 mb-2 bg-primary text-white text-center">
+           <h1>React Counter</h1>
+           <h2 class="fs-5">Please use the buttons to interact with your counter</h2>
+       </div>
     )
 }
