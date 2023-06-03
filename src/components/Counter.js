2672088e3840dfd6455ba119c649300f8ba0da8e
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 07:35:41 +0100

    created counter as a class based component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,21 @@
 
+//declaring counter as a class based component
 
-export const Counter = () =>{
+//import the react base component
+import {Component} from "react";
 
-    return(
-        <div>
-            Counter content 
-        </div>
-    )
+//create the class passing props
+export class Counter extends Component{
+    //add a constructor
+    constructor(props){
+      //call the constructor of the Component class using the super method
+      super(props);
+    }
+
+    //render method
+
+    render(){
+        return <div> Counter content </div>;
+    }
 
 }
