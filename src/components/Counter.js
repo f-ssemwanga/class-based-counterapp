d6ed77c36d86d1251d07cbc1a52e458aad8f8cac
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 10:16:58 +0100

    added states to the counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,22 +10,38 @@ export class Counter extends Component{
     constructor(props){
       //call the constructor of the Component class using the super method
       super(props);
+
+      //declare the state
+      this.state = {count:0}
     }
     //declare handler methods
     handleDecrement = () =>{
-      console.log("decrement")
+      // console.log("decrement")
+      if (this.state.count >0){
+        this.setState(
+        {count: this.state.count -1}
+      )
+      }
+      
     }
     handleIncrement = () =>{
-      console.log("increment")
+      // console.log("increment")
+      //increment the count by assessing the count variable in the state object
+      this.setState (
+        {count:this.state.count+1}
+      )
     }
     handleReset = () =>{
-      console.log("reset")
+      // console.log("reset")
+      this.setState(
+        {count:0,}
+      )
     }
     //render method
 
     render(){
         return <div className="border m-3 p-4 rounded"> 
-          <div className="sub-title text-center m-5 fs-2">Counter: <span className="text-primary fs-2 p-2 fw-bold">0</span></div>
+          <div className="sub-title text-center m-5 fs-2">Counter: <span className="text-primary fs-2 p-2 fw-bold">{this.state.count}</span></div>
           <div className="d-flex justify-content-evenly flex-column gap-3">
               <button className ="btn btn-success fs-3" onClick={this.handleDecrement}>-</button>
               <button className ="btn btn-danger" onClick={this.handleReset}>Reset</button>
