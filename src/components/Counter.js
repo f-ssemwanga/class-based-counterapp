91e3d2c9c5bc2a4b3d59b45cbb8bff35f1e614df
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 09:40:53 +0100

    connected event handler functions in the counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -11,16 +11,25 @@ export class Counter extends Component{
       //call the constructor of the Component class using the super method
       super(props);
     }
-
+    //declare handler methods
+    handleDecrement = () =>{
+      console.log("decrement")
+    }
+    handleIncrement = () =>{
+      console.log("increment")
+    }
+    handleReset = () =>{
+      console.log("reset")
+    }
     //render method
 
     render(){
         return <div className="border m-3 p-4 rounded"> 
           <div className="sub-title text-center m-5 fs-2">Counter: <span className="text-primary fs-2 p-2 fw-bold">0</span></div>
           <div className="d-flex justify-content-evenly flex-column gap-3">
-              <button className ="btn btn-success fs-3">-</button>
-              <button className ="btn btn-danger">Reset</button>
-              <button className ="btn btn-success fs-3">+</button> 
+              <button className ="btn btn-success fs-3" onClick={this.handleDecrement}>-</button>
+              <button className ="btn btn-danger" onClick={this.handleReset}>Reset</button>
+              <button className ="btn btn-success fs-3" onClick={this.handleIncrement}>+</button> 
           </div>
                               
           
