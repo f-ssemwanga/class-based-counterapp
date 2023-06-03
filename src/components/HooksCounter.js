aab7b510ca44d4aeb5f3c704ce2e0bcb659ff060
Francis Ssemwanga <122269670+f-ssemwanga@users.noreply.github.com>
2023-06-03 11:15:47 +0100

    completed Hoooks Counter Part of the React Counter

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,15 +1,34 @@
+import { useState } from "react"
 
 export const HooksCounter = ({subTitle}) =>{
+    
+    // definin a use state
+    const [count, setCount] = useState(0)
+    
+    //event handlers
+    const resetHandler = () =>{
+        setCount(0)
+    }
+
+    const incrementHandler = () =>{
+        setCount(count + 1)
+    }
+    const decrementHandler = () =>{
+        if(count >0){
+            setCount(count -1)
+        }
+        
+    }
 
     return(
         <div className="border m-3 p-4 rounded"> 
         <h2 className="text-center">{subTitle}</h2>
         <hr/>
-          <div className="sub-title text-center m-3 fs-2">Counter: <span className="text-primary fs-2 p-2 fw-bold">0</span></div>
+          <div className="sub-title text-center m-3 fs-2">Counter Value: <span className="text-primary fs-2 p-2 fw-bold">{count}</span></div>
           <div className="d-flex justify-content-evenly flex-column gap-3">
-              <button className ="btn btn-success fs-3">-</button>
-              <button className ="btn btn-danger" >Reset</button>
-              <button className ="btn btn-success fs-3">+</button> 
+              <button className ="btn btn-success fs-3" onClick={decrementHandler}>-</button>
+              <button className ="btn btn-danger"  onClick={resetHandler}>Reset</button>
+              <button className ="btn btn-success fs-3" onClick={incrementHandler}>+</button> 
           </div>
         
           </div>
